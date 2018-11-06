2c0150a630e196338584d05d446cc336c257a9bd
mattack182 <alexbohn85@gmail.com>
2018-11-06 12:33:43 -0800

    day 8 commit 2

--- day 8/js/scripts.js
+++ day 8/js/scripts.js
@@ -1,13 +1,22 @@
 function toggle(){
+	// var my_value = prompt("Give me a color name", "blue");
+	// document.getElementById("h1_2nd").innerHTML = document.getElementById("h1_2nd").innerHTML.toUpperCase();
 	
-	var pColor = document.getElementById("first").style.color;	
+	// var pColor = document.getElementById("first").style.color;	
+	// document.getElementById("first").style.color = my_value;
 
-	if(pColor == "yellow"){
-		document.getElementById("first").style.color = "blue";
-		document.getElementById("first").style.display = "block";
+	var array_items = document.getElementsByClassName("class_can_be_reused");
+
+	for(i = 0; i < array_items.length; i++){
+		array_items[i].innerHTML = "CHANGED";
+		console.log("print i " + i);
 	}
-	else{
-		document.getElementById("first").style.color = "yellow";
-		document.getElementById("first").style.display = "none";
-	}
+	// if(pColor == "yellow"){
+	// 	document.getElementById("first").style.color = "blue";
+	// 	document.getElementById("first").style.display = "block";
+	// }
+	// else{
+	// 	document.getElementById("first").style.color = "yellow";
+	// 	document.getElementById("first").style.display = "none";
+	// }
 }
